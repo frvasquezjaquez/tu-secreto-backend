047f6d935d1868d4f5047627b8d1a027cdfb3790
Francisco J. Vásquez <frvasquezjaquez@gmail.com>
2019-05-29 13:17:23 -0400

    Allow Cors

--- src/server.js
+++ src/server.js
@@ -22,6 +22,13 @@ import { ApolloServer} from 'apollo-server-express';
 import typeDefs from './modules/motel/motel.schema'
 import resolvers from './modules/motel/motel.resolver'
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const SERVER = new ApolloServer({
       typeDefs,
       resolvers,
